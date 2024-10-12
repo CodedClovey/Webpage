3e1f0c27d87e9715afae36d451b495311355fecb
CodedClovey <abhinavmanoj222@gmail.com>
2024-10-12 17:23:04 +0530

    h

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     basePath: isProd ? '/Webpage' : '',
     output: 'export',
+    images: {
+        unoptimized: true,
+    }
 };
 
 export default nextConfig;
