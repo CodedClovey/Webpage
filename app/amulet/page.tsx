ea22353502d07bfa8b668fce4ec211e21cb4b8ac
CodedClovey <abhinavmanoj222@gmail.com>
2024-10-12 16:53:36 +0530

    ch

--- app/amulet/page.tsx
+++ app/amulet/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Page() {
     return <div>
-        <Image alt="img" src='/Website/home.png'
+        <Image alt="img" src='/Webpage/home.png'
         width={428}
         height={908}
         ></Image>
