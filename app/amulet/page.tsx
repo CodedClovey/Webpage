10c08e2edee9769f6da80f349e7f740288a2dcc5
CodedClovey <abhinavmanoj222@gmail.com>
2024-10-12 16:30:25 +0530

    img

--- app/amulet/page.tsx
+++ app/amulet/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Page() {
     return <div>
-        <Image alt="img" src='/favicon.ico'
+        <Image alt="img" src='/home.png'
         width={200}
         height={200}
         ></Image>
