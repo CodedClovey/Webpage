0d6580b840271e1e2ac1a9af61a29124167c537c
CodedClovey <abhinavmanoj222@gmail.com>
2024-10-16 15:54:20 +0530

    init

--- app/amulet/page.tsx
+++ app/amulet/page.tsx
@@ -1,37 +1,71 @@
+'use client'
+
 import Image from "next/image";
+import { useState } from "react";
 
+const pjs = [
+    {
+        link: '/am1.png',
+        tag: 'Schedule',
+        desc: 'Schedule your day, break down tasks and mark completed tasks',
+    },
+    {
+        link: '/am2.png',
+        tag: 'Focus',
+        desc: 'Set up a timer and keep focused throughout the day',
+    },
+    {
+        link: '/am3.png',
+        tag: 'Notes',
+        desc: 'Note down important things or list out items and mark them',
+    },
+    {
+        link: '/am4.png',
+        tag: 'Calendar',
+        desc: 'Set Reminders so that you do not forget important events',
+    },
+]
 export default function Page() {
+    const [pos,setPos] = useState(0)
     return (
-    <div className="flex flex-col  gap-14" >
-
-        <div className="flex-none flex-col ">
-            <p className="text-2xl text-center font-[family-name:var(--font-geist-mono)]">Amulet</p>
-            <p className="text-1xl text-center font-[family-name:var(--font-geist-mono)] text-grey">Productivity App</p>
-        </div>
-    
-        <div className=" flex flex-row flex-wrap gap-5 items-center justify-center grow">
-        {['/am1.png','/am2.png','/am3.png','/am4.png',].map((item,index)=>{
-            return(
-            <div key={index} className=" flex flex-col gap-5 items-center justify-center font-[family-name:var(--font-geist-sans)] ">
-                <div className='border-4 rounded-lg border-gray-300 drop-shadow-2xl transition ease-in-out hover:scale-105'>
-                    <Image 
-                    alt="img" src={item}
-                    className=""
-                    width={418/2}
-                    height={882/2}
-                    ></Image>
-                </div>
-                <p>
-                    {item=='/am1.png' && 'Schedule your day'}
-                    {item=='/am2.png' && 'Keep focused'}
-                    {item=='/am3.png' && 'Note things down'}
-                    {item=='/am4.png' && 'Set Reminders'}
-                    
-                </p>
+    <div className="flex flex-row justify-between gap-14 px-32 py-16 grow" >
+        <div className="flex flex-col justify-between">
+            <div className="flex flex-col ">
+                <p className="text-2xl text-left font-[family-name:var(--font-geist-mono)]">Amulet</p>
+                <p className="text-1xl text-left font-[family-name:var(--font-geist-mono)] text-grey">Productivity App</p>
             </div>
-            )
-        })
-        }
+            <div>
+                <div className="my-7 ">
+                    <p className="text-1xl text-left font-[family-name:var(--font-geist-mono)] text-grey">{pjs[pos].desc}</p>
+                </div>
+                <div className="flex flex-row flex-wrap gap-3">
+                {
+                    pjs.map((item,index)=>{
+                        return(
+                            <button key={index} onClick={()=>{
+                                setPos(index)
+                            }
+                            }>
+                                <div className="bg-cyan-700 px-8 py-2 rounded-xl">
+                                    <p className="text-slate-100 font-[family-name:var(--font-geist-mono)]">{item.tag}</p>
+                                </div>
+                            </button>
+                        )
+                    })
+                }
+                </div>
+            </div>
+        </div>
+
+        <div className=" flex flex-col gap-5 items-center justify-center font-[family-name:var(--font-geist-sans)] ">
+            <div className='border-4 rounded-lg border-gray-300 drop-shadow-2xl transition ease-in-out hover:scale-105'>
+                <Image
+                alt="img" src={pjs[pos].link}
+                className=""
+                width={418/2}
+                height={882/2}
+                ></Image>
+            </div>
         </div>
 
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col font-[family-name:var(--font-geist-sans)] grow items-center justify-center">
-          <div className="flex-none flex-col">
-            <p className="text-2xl text-center font-[family-name:var(--font-geist-mono)]">Abhinav Manoj</p>
-            <p className="text-1xl text-center font-[family-name:var(--font-geist-mono)] text-grey">Front-End Developer</p>
+    <div className="flex flex-col font-[family-name:var(--font-geist-sans)] grow md:px-40">
+      <div className="flex flex-row justify-between">
+          <div className="flex flex-col">
+            <p className="text-2xl md:text-left text-center font-[family-name:var(--font-geist-mono)]">Abhinav Manoj</p>
+            <p className="text-1xl md:text-left text-center font-[family-name:var(--font-geist-mono)] text-grey">Front-End Developer</p>
           </div>
-      <div className="flex grow items-center justify-center ">
+
+          <div className="flex h-10 w-40 bg-teal-600 items-center justify-center rounded-xl self-center">
+            <p className="text-gray-100"><a href="mailto:abhinavmanoj222@gmail.com">Contact me</a></p>
+          </div>
+      </div>
+      <div className="flex grow items-center justify-center">
         <Link href={'/amulet'}>
           <div className="flex transition ease-in-out hover:scale-105 ">
                   <Image
@@ -20,7 +28,7 @@ export default function Home() {
           </div>
         </Link>
       </div>
-      <p className="text-gray-500"><a href="mailto:abhinavmanoj222@gmail.com">Contact me</a></p>
+
     </div>
   );
 }
